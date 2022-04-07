46cf1ed4a49dfe4090fe0f6b953c60688b6ab7b3
w1shm3luck <alanwydianto@gmail.com>
2022-04-07 10:20:31 +0700

    finpro-first

---
+++ FINPRO/Keep-notes-apps/react-notes-app/src/App.js
@@ -0,0 +1,77 @@
+import {  useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
+import Search from './components/Search';
+import Header from './components/Header';
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+    id: nanoid(),
+    text: "Note Apps using React JS(1)!",
+    date: "30/03/2022",
+    },
+    {
+      id: nanoid(),
+      text: "Note Apps using React JS(2)!",
+      date: "30/03/2022",
+    },
+    {
+      id: nanoid(),
+      text: "Note Apps using React JS(3)!",
+      date: "30/03/2022",
+    },
+    {
+      id: nanoid(),
+      text: "(NEW) Note Apps using React JS(4)!",
+      date: "30/03/2022",
+    },
+  ]);
+
+  const [searchText, setSearchText] = useState('');
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+      localStorage.setItem(
+        'react-notes-app-data', 
+        JSON.stringify(notes) 
+      );
+  }, [notes]);
+  
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString()
+    }
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+      const newNotes = notes.filter((note)=> note.id !== id);
+      setNotes(newNotes);
+  }
+
+  return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+    <div className='container'>
+    <Header handleToggleDarkMode={setDarkMode} />
+    <Search handleSearchNote={setSearchText}  />
+    <NotesList 
+    notes={notes.filter((note)=> 
+      note.text.toLowerCase().includes(searchText)
+    )} 
+    handleAddNote={ addNote }
+    handleDeleteNote= { deleteNote }
+    />
+  </div>
+  </div>
+  );
+};
+
+export default App;
+
